b11e7acf8b68e8ed693b15d78520e6a90809e033
rmacuna <labcienciascomputacion8@labcienciascomputacion8s-iMac.local>
2017-10-17 10:45:04 -0500

    Prueba2 PHP

--- Reservas/enviar.php
+++ Reservas/enviar.php
@@ -1,27 +1,31 @@
 <?php
-  echo $_POST[nombre];
-  // $destino="dolceymendozajr@gmail.com";
-  // $nombre=$_POST["nombre"];
-  // $asunto="Reserva - ".$nombre;
-  // $mail=$_POST["mail"];
-  // $telefono=$_POST["telefono"];
-  // $cantidad=$_POST["cper"];
-  // $descrip=$_POST["desc"];
-  // $chkin=$_POST["chkin"];
-  // $chkout=$_POST["chkout"];
-  //
-  // if (isset($cantidad,$descrip)) {
-  //   $contenido="----RESERVA DE SALON REAL----\n\n"."\nNombre de persona encargada: ".$nombre
-  //   ."\nMail de contacto: ".$mail."\nTelefono: ".$telefono
-  //   ."\nCantidad de personas en evento: ".$cantidad."\n Descripción del evento: ".$descrip
-  //   ."\nCheck-in: ".$chkin."\nCheck-out: ".$chkout;
-  // }else {
-  //   $contenido=$contenido="----RESERVA DE HABITACIÓN----"
-  //   ."\nNombre del cliente: ".$nombre
-  //   ."\nMail de contacto: ".$mail."\nTelefono: ".$telefono
-  //   ."\nCheck-in: ".$chkin."\nCheck-out: ".$chkout;
-  // }
-  //
+  $destino="dolceymendozajr@gmail.com";
+  $nombre=$_POST["nombre"];
+  $asunto="Reserva - ".$nombre;
+  $mail=$_POST["mail"];
+  $telefono=$_POST["telefono"];
+  if (isset($_POST["cper"])) {
+    $cantidad=$_POST["cper"];
+  }
+  if (isset($_POST["desc"])) {
+    $descrip=$_POST["desc"];
+  }
+  $chkin=$_POST["chkin"];
+  $chkout=$_POST["chkout"];
+
+  if (isset($cantidad,$descrip)) {
+    $contenido="----RESERVA DE SALON REAL----\n\n"."\nNombre de persona encargada: ".$nombre
+    ."\nMail de contacto: ".$mail."\nTelefono: ".$telefono
+    ."\nCantidad de personas en evento: ".$cantidad."\n Descripción del evento: ".$descrip
+    ."\nCheck-in: ".$chkin."\nCheck-out: ".$chkout;
+  }else {
+    $contenido=$contenido="----RESERVA DE HABITACIÓN----"
+    ."\nNombre del cliente: ".$nombre
+    ."\nMail de contacto: ".$mail."\nTelefono: ".$telefono
+    ."\nCheck-in: ".$chkin."\nCheck-out: ".$chkout;
+  }
+echo $contenido ;
+
   // mail($destino,$asunto,$contenido);
   // header("Location:reservas.html")
 
