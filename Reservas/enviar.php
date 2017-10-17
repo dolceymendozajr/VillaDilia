ef9f036cd6cc0fe3a40c509ab3305936a141d748
rmacuna <labcienciascomputacion8@labcienciascomputacion8s-iMac.local>
2017-10-17 10:30:29 -0500

    Prueba PHP

--- Reservas/enviar.php
+++ Reservas/enviar.php
@@ -1,28 +1,29 @@
 <?php
-  $destino="dolceymendozajr@gmail.com";
-  $nombre=$_POST["nombre"];
-  $asunto="Reserva - ".$nombre;
-  $mail=$_POST["mail"];
-  $telefono=$_POST["telefono"];
-  $cantidad=$_POST["cper"];
-  $descrip=$_POST["desc"];
-  $chkin=$_POST["chkin"];
-  $chkout=$_POST["chkout"];
-
-  if (isset($cantidad,$descrip)) {
-    $contenido="----RESERVA DE SALON REAL----\n\n"."\nNombre de persona encargada: ".$nombre
-    ."\nMail de contacto: ".$mail."\nTelefono: ".$telefono
-    ."\nCantidad de personas en evento: ".$cantidad."\n Descripción del evento: ".$descrip
-    ."\nCheck-in: ".$chkin."\nCheck-out: ".$chkout;
-  }else {
-    $contenido=$contenido="----RESERVA DE HABITACIÓN----"
-    ."\nNombre del cliente: ".$nombre
-    ."\nMail de contacto: ".$mail."\nTelefono: ".$telefono
-    ."\nCheck-in: ".$chkin."\nCheck-out: ".$chkout;
-  }
-
-  mail($destino,$asunto,$contenido);
-  header("Location:reservas.html")
+  echo $_POST[nombre];
+  // $destino="dolceymendozajr@gmail.com";
+  // $nombre=$_POST["nombre"];
+  // $asunto="Reserva - ".$nombre;
+  // $mail=$_POST["mail"];
+  // $telefono=$_POST["telefono"];
+  // $cantidad=$_POST["cper"];
+  // $descrip=$_POST["desc"];
+  // $chkin=$_POST["chkin"];
+  // $chkout=$_POST["chkout"];
+  //
+  // if (isset($cantidad,$descrip)) {
+  //   $contenido="----RESERVA DE SALON REAL----\n\n"."\nNombre de persona encargada: ".$nombre
+  //   ."\nMail de contacto: ".$mail."\nTelefono: ".$telefono
+  //   ."\nCantidad de personas en evento: ".$cantidad."\n Descripción del evento: ".$descrip
+  //   ."\nCheck-in: ".$chkin."\nCheck-out: ".$chkout;
+  // }else {
+  //   $contenido=$contenido="----RESERVA DE HABITACIÓN----"
+  //   ."\nNombre del cliente: ".$nombre
+  //   ."\nMail de contacto: ".$mail."\nTelefono: ".$telefono
+  //   ."\nCheck-in: ".$chkin."\nCheck-out: ".$chkout;
+  // }
+  //
+  // mail($destino,$asunto,$contenido);
+  // header("Location:reservas.html")
 
 
  ?>
